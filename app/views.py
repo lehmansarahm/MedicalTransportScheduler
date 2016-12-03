6a86c0f280c9047016fc50e42c71fd4bf3160758
Sarah Lehman <tug46038@temple.edu>
2016-12-03 17:37:02 -0500

    Placeholder pages for schedule / reports

--- app/views.py
+++ app/views.py
@@ -8,20 +8,29 @@ from .forms import LoginForm
 @app.route('/')
 @app.route('/index')
 def index():
-	user = {'nickname':'Sarah'}	#fake user
-	posts = [
-		{'author':{'nickname':'Stephen'},'body':'The database design is in progress!'},
-		{'author':{'nickname':'Dusan'},'body':'The scheduling algorithm is in progress!'}
-	]
-	return render_template('index.html',title='Home',user=user,posts=posts)
+	return render_template('index.html',title='Home')
 
 # -----------------------------------------------------------------------------
 # -----------------------------------------------------------------------------
 
-@app.route('/login',methods=['GET','POST'])
-def login():
-	form = LoginForm()
-	if form.validate_on_submit():
-		flash('Login requested for OpenID="%s", remember_me=%s' % (form.openid.data, str(form.remember_me.data)))
-		return redirect('/index')
-	return render_template('login.html',title='Sign In',form=form,providers=app.config['OPENID_PROVIDERS'])
+@app.route('/schedule')
+def schedule():
+	return render_template('schedule.html',title='Driving Schedule')
+
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+
+@app.route('/reports')
+def reports():
+	return render_template('reports.html',title='Scheduler Reports')
+
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+
+#@app.route('/login',methods=['GET','POST'])
+#def login():
+#	form = LoginForm()
+#	if form.validate_on_submit():
+#		flash('Login requested for OpenID="%s", remember_me=%s' % (form.openid.data, str(form.remember_me.data)))
+#		return redirect('/index')
+#	return render_template('login.html',title='Sign In',form=form,providers=app.config['OPENID_PROVIDERS'])
